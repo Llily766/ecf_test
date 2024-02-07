910196d5ce060cef2e35914f4a0902280f339bec
BINET aurélie <au.binet21@gmail.com>
2024-02-07 18:33:50 +0100

    Ajout d'un garage et d'un service

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use App\Entity\Hours;
+use App\Entity\Service;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index (EntityManagerInterface $entityManager): Response 
     {
+        $services = $entityManager->getRepository(Service::class)->findAll();
+        // $hours = $entityManager->getRepository(Hours::class)->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            // 'hours'=> $hours,
+            'services' => $services ,
+        
         ]);
     }
+
+    
 }
